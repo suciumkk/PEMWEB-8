8d36ae75513fc84bf0f5e8f0dff0f889dcb930f0
suciumkk <75971664+suciumkk@users.noreply.github.com>
2021-05-03 18:45:39 +0700

    Add files via upload

---
+++ formvalidation3.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/boostrap/4.1.3/css/boostrap.min.css" integrity="sha384-MC-w98/5FnGE8fJT3GXwEOngsv7Zt27NXFoaoApmYm8liuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<style>
+		.warning {color: #FF0000;}
+	</style>
+</head>
+<body>
+<?php  
+$error_nama ="";
+$error_email ="";
+$error_web ="";
+$error_pesan ="";
+$error_telp ="";
+
+$nama ="";
+$email ="";
+$web ="";
+$pesan ="";
+$telp ="";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if(empty($_POST["nama"]))
+	{
+		$error_nama = "Nama Tidak Boleh Kosong";
+	}
+	else 
+	{
+		$nama = cek_input($_POST["nama"]);
+		if(!preg_match("/^[a-zA-Z]*$/",$nama))
+		{
+			$namaErr = "Inputan Hanya boleh Huruf dan spasi";
+		}
+	}
+	if(empty($_POST["email"]))
+	{
+		$error_email = "Email Tidak Boleh Kosong";
+	}
+	else
+	{
+		$email=cek_input($_POST["email"]);
+		if(!filter_var($email,FILTER_VALIDATE_EMAIL))
+		{
+			$error_email = "Format Email Invalid";
+		}
+	}
+	if(empty($_POST["pesan"]))
+	{
+		$error_pesan = "Pesan Tidak Boleh Kosong";
+	}
+	else
+	{
+		$pesan = cek_input($_POST["pesan"]);
+	}
+	if(empty($_POST["web"]))
+	{
+		$error_web = "Website Tidak Boleh Kosong";
+	}
+	else
+	{
+		$web = cek_input($_POST["web"]);
+		if(!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%?=~_|]/i",$web))
+		{
+			$error_web = "URL Tidak Valid";
+		}
+	}
+	if(empty($_POST["telp"]))
+	{
+		$error_telp = "Telp Tidak Boleh Kosong";
+	}
+	else
+	{
+		$telp = cek_input($_POST["telp"]);
+
+		if(!is_numeric($telp))
+		{
+			$error_telp	= 'Nomor HP Hanya Boleh Angka';
+		}
+	}
+}
+
+function cek_input($data){
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+?>
+<div class="row">
+<div class="col-md-6">
+<div class="card">
+	<div class="card-header">
+		Contoh Validasi Form Dengan PHP
+	</div>
+	<div class="card-body">
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> <div class="form-group row">
+	<label for="nama" class="col-sm-2 col-form-label">Nama</label>
+	<div class="col-sm-10">
+		<input type="text" name="nama" class="form-control <?php echo ($error_nama !="" ? "is-invalid" : ""); ?>" id="nama" placeholder="Nama" value="<?php echo $nama; ?>"><span class="warning"><?php echo $error_nama; ?></span>
+	</div>	
+  </div>
+
+<div class="form-group row">
+	<label for="email" class="col-sm-2 col-form-label">Email</label>
+	<div class="col-sm-10">
+		<input type="text" name="email" class="form-control <?php echo($error_email !="" ? "is-invalid" : ""); ?>" id="email " placeholder="email" value="<?php echo $email; ?>"><span class="warning"><?php echo $error_email; ?></span>
+	</div>
+</div>
+
+<div class="form-group row">
+	<label for="web" class="col-sm-2 col-form-label">Website</label>
+	<div> 
+		<input type="text" name="web" class="form-control <?php echo($error_web !="" ? "is-invalid" : ""); ?>" id="web" placeholder="web" value="<?php echo $web; ?>"><span class="warning"><?php echo $error_web; ?></span>
+	</div>
+</div>
+
+<div class="form-group row">
+	<label for="telp" class="col-sm-2 col-form-label">Telp</label>
+	<div class="col-sm-10">
+		<input type="text" name="telp" class="form-control <?php echo($error_telp !="" ? "is-invalid" : ""); ?>" id="telp" placeholder="telp" value="<?php echo $telp; ?>"><span class="warning"><?php echo $error_telp; ?></span>
+	</div>
+</div>
+
+<div class="form-group row">
+	<label for="pesan" class="col-sm-2 col-form-label">Pesan</label>
+	<div class="col-sm-10">
+		<textarea name="pesan" class="form-control <?php echo ($error_pesan !="" ? "is-invalid" : ""); ?>"><?php echo $pesan; ?></textarea><span class="warning"><?php echo $error_pesan; ?></span>
+	</div>
+</div>
+
+<div class="form-group">
+	<div class="col-sm-10">
+		<button type="submit" class="btn btn-primary">Sig in</button>
+	</div>
+</div>
+</form>
+	</div>
+</div>
+</div>
+</div>
+
+<?php 
+echo "<h2>Your Input :</h2>";
+echo "Nama = ".$nama;
+echo "<br>";
+echo "Email = ".$email;
+echo "<br>";
+echo "Website = ".$web;
+echo "<br>";
+echo "Telp = ".$telp;
+echo "<br>";
+echo "Pesan = ".$pesan;
+?>
+
+</body>
+</html>
